a1dfa599bf9f115a7676901eddf7933131e11a79
Daniel Bingham <danielbingham000@gmail.com>
2026-05-01 12:09:48 -0500

    chore: updated app.errorhandler

--- app/views.py
+++ app/views.py
@@ -60,4 +60,4 @@ def add_header(response):
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
-    return render_template('404.html'), 404
+    return jsonify(message="Page not found"), 404
