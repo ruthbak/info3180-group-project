9e12a808934aabd13b332fa9ceb25b390d2c846a
beatrizJ-P <beatrice.matthias@mymona.uwi.edu>
2026-05-01 14:46:00 -0500

    feat: assisted in creating forms, editing init_py,  views setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from .config import Config
+from flask_migrate import Migrate
+from flask_sqllchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 app.config.from_object(Config)
 
-from app import views
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import views, models
