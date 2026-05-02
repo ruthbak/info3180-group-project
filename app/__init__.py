26c48f2402c42c01b3d69b76cffab5eb50917787
Daniel Bingham <danielbingham000@gmail.com>
2026-05-02 18:28:35 -0500

    chore: updated __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect 
 
 app = Flask(__name__)
+app.config.from_object(Config)
 csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-app.config.from_object(Config)
+
 
 from app import views
