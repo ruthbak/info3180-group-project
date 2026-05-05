2022b6fcd7c71d49934331cbf2261113022c8c8f
Rashene Dillon <rashwork2024@gmail.com>
2026-05-05 11:58:51 -0500

    feats: Updates front-end with more styling

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,14 @@ from .config import Config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config.from_object(Config) 
 csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-app.config.from_object(Config)
 
 from app import views
